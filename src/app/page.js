fa80fb58d1f5bec497dd8e629a049b6ca119a0c2
Celia <cmarianielsen@gmail.com>
2026-04-13 09:32:20 +0200

    search done

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
 import Image from "next/image";
+import { LiaSearchSolid } from "react-icons/lia";
+import { CgSearch } from "react-icons/cg";
 
 export default function Home() {
-	return <section className=" grid grid-cols-[repeat(auto-fill.minmax(250px,1fr))] gap-4"> </section>;
+	return (
+		<section className=" grid grid-cols-[repeat(auto-fill.minmax(250px,1fr))] gap-4">
+			<div className="flex justify-between my-6">
+				<LiaSearchSolid size={45} color="white" className="bg-[#F2968F] p-2 rounded-full" />
+				<input
+					type="search"
+					id="site-search"
+					name="q"
+					placeholder="search breeds"
+					className="border-[1.5px] text-[#CACACD] rounded-full px-6 py-2"
+				/>
+			</div>
+		</section>
+	);
 }
