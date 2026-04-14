ef2ab93f4afa754e5d2073896b553e53845d4be3
Celia <cmarianielsen@gmail.com>
2026-04-14 18:37:44 +0200

    fikset lidt css

--- src/app/components/BreedCard.jsx
+++ src/app/components/BreedCard.jsx
@@ -3,10 +3,16 @@ import { CiStar } from "react-icons/ci";
 
 const BreedCard = ({ breedGroup, origin, image }) => {
 	return (
-		<div className=" bg-white rounded-2xl shadow mb-4 w-fit">
+		<div className=" bg-white rounded-2xl shadow mb-4 w-fit ">
 			<div className="relative">
 				<CiStar size={24} color="white" className="bg-[rgba(254,254,254,0.25)] absolute m-2 rounded-full right-0" />
-				<Image src={image} alt="lil bean" width={160} height={114} className="rounded-2xl object-cover" />
+				<Image
+					src={image}
+					alt="lil bean"
+					width={160}
+					height={114}
+					className="rounded-2xl object-cover aspect-square"
+				/>
 			</div>
 			<div className="m-3">
 				<h3 className="text-[#333333] text-base font-bold">{breedGroup}</h3>
--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ export default function Home() {
 	return (
 		<section className=" grid grid-cols-[repeat(auto-fill.minmax(250px,1fr))] gap-4">
 			<Searchbar />
-			<BreedSingleView />
 			<section className="flex flex-wrap gap-7 justify-center mb-3">
 				<BreedList />
 			</section>
